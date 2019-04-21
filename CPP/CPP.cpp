093de9e446465c0d9de333edd985ac308daff23c
wangc2h4 <s0up1ness@qq.com>
2019-04-22 03:21:18 +0800

    3.3.3 for(auto &a:b）
    &a引用了列表b的对象，当a改变时，列表随之更新

---
+++ CPP/CPP.cpp
@@ -0,0 +1,18 @@
+﻿// CPP.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> v{ 1,2,3,4,5,6,7,8,9 };
+	for (auto& i : v)
+		i *= i;  //把更新后的i值返回更新列表V
+	for (auto i : v)
+		cout << i << " ";
+	cout << endl;
+		return 0;
+}
+
